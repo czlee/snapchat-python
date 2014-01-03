fcd81fd9c6050dc4141cb89fd67882da1829192c
Chuan-Zheng Lee <cz.lee11@gmail.com>
2014-01-02 17:47:52 -0800

    Take note of sender and timestamp and use them in the filenames

--- snapchat.py
+++ snapchat.py
@@ -58,30 +58,34 @@ def sync(auth_token, username):
     return r.text
 
 
-def save_img(raw, img_id):
-    test_file = open(str(img_id) + ".jpg", "w")
+def save_img(raw, img_id, target=None):
+    if target is None:
+        target = img_id
+    test_file = open(str(target) + ".jpg", "w")
     test_file.write(raw)
     test_file.close()
 
     return True
 
 
-def save_vid(raw, vid_id):
-    test_file = open(str(vid_id) + ".mp4", "w")
+def save_vid(raw, vid_id, target=None):
+    if target is None:
+        target = img_id
+    test_file = open(str(target) + ".mp4", "w")
     test_file.write(raw)
     test_file.close()
 
     return True
 
 
-def download_and_decrypt_url(img_url, item_id, is_vid):
+def download_and_decrypt_url(img_url, item_id, is_vid, target=None):
     img_string = requests.get(img_url).content
     cipher = AES.new(ENCRYPT_KEY_2)
 
     raw = cipher.decrypt(pad(img_string))
     if is_vid:
-        return save_vid(raw, item_id)
-    return save_img(raw, item_id)
+        return save_vid(raw, item_id, target)
+    return save_img(raw, item_id, target)
 
 
 def get_url(img_id, username, auth_token):
@@ -91,15 +95,18 @@ def get_url(img_id, username, auth_token):
     return url
 
 
-def get_image(img_id, username, auth_token):
+def get_image(img_id, username, auth_token, target=None):
     url = get_url(img_id, username, auth_token)
-    return download_and_decrypt_url(url, img_id, False)
+    return download_and_decrypt_url(url, img_id, False, target)
 
 
-def get_video(vid_id, username, auth_token):
+def get_video(vid_id, username, auth_token, target=None):
     url = get_url(vid_id, username, auth_token)
-    return download_and_decrypt_url(url, vid_id, True)
+    return download_and_decrypt_url(url, vid_id, True, target)
 
+
+def time_filename(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime("%Y%m%dT%H%M%S")
 
 def download_unread(username, password):
     auth_token = json.loads(login(username, password))['auth_token']
@@ -110,12 +117,17 @@ def download_unread(username, password):
         if 'm' not in snap or 'rp' in snap:
             # Either the format has changed, or this was a sent snap.
             continue
+        timestamp = time.localtime(snap['sts'] / 1000)
+        sender = snap['sn']
+        message = "getting {0} from " + sender + " at " + time.asctime(timestamp) + ": " + snap['id']
+        target = time.strftime("%Y%m%dT%H%M%S", timestamp) + "_" + sender
+
         if snap['m'] == 0 and snap['st'] == 1:
-            print "getting pic: " + snap['id']
-            get_image(snap['id'], username, auth_token)
+            print message.format("pic")
+            get_image(snap['id'], username, auth_token, target)
         elif snap['m'] == 1 and snap['st'] == 1:
-            print "getting video: " + snap['id']
-            get_video(snap['id'], username, auth_token)
+            print message.format("video")
+            get_video(snap['id'], username, auth_token, target)
 
 
 if __name__ == "__main__":
